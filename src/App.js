88e0354a17c37a9156625225d858face414ac664
aa08d <alisultanovdzamaludin0@gmail.com>
2025-07-31 14:00:42 +0300

    second

--- src/App.js
+++ src/App.js
@@ -3,45 +3,45 @@ import React, { useEffect } from 'react';
 const telegramBotUsername = 'GadVPNBot';
 
 function App() {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    // const targetUrl = params.get('target');
-    const targetUrl = "v2raytun://import/https://gadvpn.ru/sub/dXNlcl83NDIxNDkxNjU3LDE3NTM4ODEzNjMvUntg8lWB1#GadVPN"
+  	useEffect(() => {
+		const params = new URLSearchParams(window.location.search);
+		const targetUrl = params.get('target');
+		
+		if (targetUrl) {
+      		window.location.href = `v2raytun://import/${targetUrl}#GadVPN`;
+    	}
+  	}, []);
 
-      // Выполняем редирект сразу
-    window.location.href = targetUrl;
-  }, []);
-
-  return (
-    <div style={styles.container}>
-      <h2>Сейчас вы будете перенаправлены...</h2>
-      <a
-        href={`https://t.me/${telegramBotUsername}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        style={styles.button}
-        aria-label="Перейти в Telegram бот"
-      >
-        {/* Иконка Telegram SVG */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="#fff"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          style={{ marginRight: 8 }}
-        >
-          <path d="M22 2L11 13"></path>
-          <path d="M22 2L15 22 11 13 2 9 22 2z"></path>
-        </svg>
-        Написать в Telegram
-      </a>
-    </div>
-  );
+  	return (
+		<div style={styles.container}>
+		<h2>Сейчас вы будете перенаправлены...</h2>
+		<a
+			href={`https://t.me/${telegramBotUsername}`}
+			target="_blank"
+			rel="noopener noreferrer"
+			style={styles.button}
+			aria-label="Перейти в Telegram бот"
+		>
+			{/* Иконка Telegram SVG */}
+			<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="#fff"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			style={{ marginRight: 8 }}
+			>
+			<path d="M22 2L11 13"></path>
+			<path d="M22 2L15 22 11 13 2 9 22 2z"></path>
+			</svg>
+			Написать в Telegram
+		</a>
+		</div>
+  	);
 }
 
 const styles = {
